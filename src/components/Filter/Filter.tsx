904221dbd72c00ab1afdb5308bae87858b8392a9
Miguel Lopez <miguelalopezg96@gmail.com>
2020-07-26 17:27:34 -0500

    Add conditional when props are null or zero

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -3,12 +3,15 @@ import { FilterProps } from './Filter.model';
 import './Filter.scss';
 
 const Filter = ({ filters }: FilterProps) => {
+  if (!filters || !filters.length) {
+    return null;
+  }
   return (
     <section className='filter'>
       <div className='filter-container'>
         <ul>
-          {filters.map((value) => (
-            <li>
+          {filters.map((value, index) => (
+            <li key={index}>
               <p>{value}</p>
               <button type='button'>
                 <span>&#215;</span>
