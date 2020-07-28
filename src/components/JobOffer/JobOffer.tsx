5c3cc26fff0ecccce1e7bfa6e4403aa63fd7d675
Miguel Lopez <miguelalopezg96@gmail.com>
2020-07-27 19:23:57 -0500

    Add box-shadow to job-offer-container css class

--- src/components/JobOffer/JobOffer.tsx
+++ src/components/JobOffer/JobOffer.tsx
@@ -4,7 +4,7 @@ import './JobOffer.scss';
 const JobOffer = () => {
   return (
     <>
-      <section className='job-offer-container'>
+      <section className='job-offer-container is-featured'>
         <div className='enterprise-logo'>
           <img
             src='https://i.picsum.photos/id/1058/200/300.jpg?hmac=KdLDQtUUGPBshA5WQaD3nZMUtdgHS9zJJ3pfoXWdjUE'
@@ -18,6 +18,35 @@ const JobOffer = () => {
             <span className='new'>New!</span>
 
             <span className='featured'>Featured</span>
+          </div>
+          <h1>Full Stack Eginner</h1>
+          <div className='job-offer-details'>
+            <ul>
+              <li>2w ago</li>
+              <li>Full time</li>
+              <li>Worldwide</li>
+            </ul>
+          </div>
+        </div>
+        <div className='job-offer-tags'>
+          <button type='button'>Frontend</button>
+          <button type='button'>Senior</button>
+          <button type='button'>JavaScript</button>
+          <button type='button'>CSS</button>
+          <button type='button'>HTML</button>
+        </div>
+      </section>
+
+      <section className='job-offer-container'>
+        <div className='enterprise-logo'>
+          <img
+            src='https://i.picsum.photos/id/1058/200/300.jpg?hmac=KdLDQtUUGPBshA5WQaD3nZMUtdgHS9zJJ3pfoXWdjUE'
+            alt=''
+          />
+        </div>
+        <div className='enterprise-description'>
+          <div className='enterprise-highlight'>
+            <h2>Eyecam Co.</h2>
           </div>
           <h1>Full Stack Eginner</h1>
           <div className='job-offer-details'>
