7316d8cf00e2eec4318373686f63050cf41639bb
Miguel Lopez <miguelalopezg96@gmail.com>
2020-07-27 23:07:23 -0500

    Separation of HTML elements

--- src/components/JobOffer/JobOffer.tsx
+++ src/components/JobOffer/JobOffer.tsx
@@ -1,102 +1,27 @@
 import React from 'react';
+import JobDescription, {
+  JobDescriptionProps,
+} from './JobDescription/JobDescription';
 import './JobOffer.scss';
+import JobTags from './JobTags/JobTags';
+import Logo, { LogoProps } from './Logo/Logo';
 
-const JobOffer = () => {
+export class JobOfferProps {
+  id: number;
+  logo: LogoProps;
+  jobDescription: JobDescriptionProps;
+  tags: string[];
+}
+
+const JobOffer = ({ logo, jobDescription, tags }: JobOfferProps) => {
+  const { isFeatured } = jobDescription;
+
   return (
-    <>
-      <section className='job-offer-container is-featured'>
-        <div className='enterprise-logo'>
-          <img
-            src='https://i.picsum.photos/id/1058/200/300.jpg?hmac=KdLDQtUUGPBshA5WQaD3nZMUtdgHS9zJJ3pfoXWdjUE'
-            alt=''
-          />
-        </div>
-        <div className='enterprise-description'>
-          <div className='enterprise-highlight'>
-            <h2>Eyecam Co.</h2>
-
-            <span className='new'>New!</span>
-
-            <span className='featured'>Featured</span>
-          </div>
-          <h1>Full Stack Eginner</h1>
-          <div className='job-offer-details'>
-            <ul>
-              <li>2w ago</li>
-              <li>Full time</li>
-              <li>Worldwide</li>
-            </ul>
-          </div>
-        </div>
-        <div className='job-offer-tags'>
-          <button type='button'>Frontend</button>
-          <button type='button'>Senior</button>
-          <button type='button'>JavaScript</button>
-          <button type='button'>CSS</button>
-          <button type='button'>HTML</button>
-        </div>
-      </section>
-
-      <section className='job-offer-container'>
-        <div className='enterprise-logo'>
-          <img
-            src='https://i.picsum.photos/id/1058/200/300.jpg?hmac=KdLDQtUUGPBshA5WQaD3nZMUtdgHS9zJJ3pfoXWdjUE'
-            alt=''
-          />
-        </div>
-        <div className='enterprise-description'>
-          <div className='enterprise-highlight'>
-            <h2>Eyecam Co.</h2>
-
-            <span className='new'>New!</span>
-          </div>
-          <h1>Full Stack Eginner</h1>
-          <div className='job-offer-details'>
-            <ul>
-              <li>2w ago</li>
-              <li>Full time</li>
-              <li>Worldwide</li>
-            </ul>
-          </div>
-        </div>
-        <div className='job-offer-tags'>
-          <button type='button'>Frontend</button>
-          <button type='button'>Senior</button>
-          <button type='button'>JavaScript</button>
-          <button type='button'>CSS</button>
-          <button type='button'>HTML</button>
-        </div>
-      </section>
-
-      <section className='job-offer-container'>
-        <div className='enterprise-logo'>
-          <img
-            src='https://i.picsum.photos/id/1058/200/300.jpg?hmac=KdLDQtUUGPBshA5WQaD3nZMUtdgHS9zJJ3pfoXWdjUE'
-            alt=''
-          />
-        </div>
-        <div className='enterprise-description'>
-          <div className='enterprise-highlight'>
-            <h2>Eyecam Co.</h2>
-          </div>
-          <h1>Full Stack Eginner</h1>
-          <div className='job-offer-details'>
-            <ul>
-              <li>2w ago</li>
-              <li>Full time</li>
-              <li>Worldwide</li>
-            </ul>
-          </div>
-        </div>
-        <div className='job-offer-tags'>
-          <button type='button'>Frontend</button>
-          <button type='button'>Senior</button>
-          <button type='button'>JavaScript</button>
-          <button type='button'>CSS</button>
-          <button type='button'>HTML</button>
-        </div>
-      </section>
-    </>
+    <section className={'job-offer-container ' + (isFeatured && 'is-featured')}>
+      <Logo {...logo} />
+      <JobDescription {...jobDescription} />
+      <JobTags tags={tags} />
+    </section>
   );
 };
 
