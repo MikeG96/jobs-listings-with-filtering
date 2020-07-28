93deb0f695f351fcd482a5c8b9f3317cfb38baca
Miguel Lopez <miguelalopezg96@gmail.com>
2020-07-27 19:17:43 -0500

    Add span new and featured in enterprise name

--- src/components/JobOffer/JobOffer.tsx
+++ src/components/JobOffer/JobOffer.tsx
@@ -3,34 +3,40 @@ import './JobOffer.scss';
 
 const JobOffer = () => {
   return (
-    <section className='job-offer-container'>
-      <div className='enterprise-logo'>
-        <img
-          src='https://i.picsum.photos/id/1058/200/300.jpg?hmac=KdLDQtUUGPBshA5WQaD3nZMUtdgHS9zJJ3pfoXWdjUE'
-          alt=''
-        />
-      </div>
-      <div className='enterprise-description'>
-        <div className='enterprise-highlight'>
-          <h2>Eyecam Co.</h2>
+    <>
+      <section className='job-offer-container'>
+        <div className='enterprise-logo'>
+          <img
+            src='https://i.picsum.photos/id/1058/200/300.jpg?hmac=KdLDQtUUGPBshA5WQaD3nZMUtdgHS9zJJ3pfoXWdjUE'
+            alt=''
+          />
         </div>
-        <h1>Full Stack Eginner</h1>
-        <div className='job-offer-details'>
-          <ul>
-            <li>2w ago</li>
-            <li>Full time</li>
-            <li>Worldwide</li>
-          </ul>
+        <div className='enterprise-description'>
+          <div className='enterprise-highlight'>
+            <h2>Eyecam Co.</h2>
+
+            <span className='new'>New!</span>
+
+            <span className='featured'>Featured</span>
+          </div>
+          <h1>Full Stack Eginner</h1>
+          <div className='job-offer-details'>
+            <ul>
+              <li>2w ago</li>
+              <li>Full time</li>
+              <li>Worldwide</li>
+            </ul>
+          </div>
         </div>
-      </div>
-      <div className='job-offer-tags'>
-        <button type='button'>Frontend</button>
-        <button type='button'>Senior</button>
-        <button type='button'>JavaScript</button>
-        <button type='button'>CSS</button>
-        <button type='button'>HTML</button>
-      </div>
-    </section>
+        <div className='job-offer-tags'>
+          <button type='button'>Frontend</button>
+          <button type='button'>Senior</button>
+          <button type='button'>JavaScript</button>
+          <button type='button'>CSS</button>
+          <button type='button'>HTML</button>
+        </div>
+      </section>
+    </>
   );
 };
 
